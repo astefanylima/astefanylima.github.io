ecbfca47ef2215defc90bdd2c70a89224ad69a08
StéfanyLima <72056277+stefany225@users.noreply.github.com>
2022-02-23 23:30:23 -0300

    Atualização

--- assets/js/scrollReveal.js
+++ assets/js/scrollReveal.js
@@ -9,5 +9,5 @@ export function initScrollReveal() {
   sr.reveal(".delayMediumReveal", { delay: 300 });
   sr.reveal(".delayLargeReveal", { delay: 400 });
   sr.reveal(".delayExtraBigReveal", { delay: 600 });
-  sr.reveal(".intervalCardReveal", { interval: 400 });
+  sr.reveal(".intervalCardReveal", { interval: 400});
 }
